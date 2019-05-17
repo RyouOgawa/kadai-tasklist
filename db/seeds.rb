d359e0146a6dfecbc0124d1a7dd5b90d1384f3c3
ryou.ogawa <ry88.buddy@gmail.com>
2019-05-17 12:28:07 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..100).each do |number|
+    Task.create(status: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
+end
+
