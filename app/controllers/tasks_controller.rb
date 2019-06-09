5c10621a9b768e902760910b9af44105acdb1d1f
ryou.ogawa <ry88.buddy@gmail.com>
2019-06-09 08:49:06 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,20 @@
 class TasksController < ApplicationController
-  #before_action :require_user_logged_in
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
   end
 
   def show
   end
 
   def new
-      @task = Task.new
+      @task = current_user.tasks.new
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
       
       if @task.save
         flash[:success] = 'Task が正常に投稿されました'
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-      @task = Tasks.find(params[:id])
+      @task = current_user.tasks.find(params[:id])
   end
     
   def task_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_101856) do
+ActiveRecord::Schema.define(version: 2019_06_09_072704) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_06_05_101856) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
