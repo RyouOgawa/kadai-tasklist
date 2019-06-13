f35483dec68ccc5d62ffb42e9ab1be4c85d55a0b
ryou.ogawa <ry88.buddy@gmail.com>
2019-06-13 12:33:45 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -10,7 +11,7 @@ class TasksController < ApplicationController
   end
 
   def new
-      @task = current_user.tasks.new
+    @task = current_user.tasks.new
   end
 
   def create
@@ -40,19 +41,25 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
-  def set_task
-      @task = current_user.tasks.find(params[:id])
-  end
-    
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+  def set_task
+      @task = current_user.tasks.find(params[:id])
   end
   
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
+  
   def index
+    if logged_in?
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
+    end
   end
 end
